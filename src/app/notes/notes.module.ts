cb913feb55adba1359c0cf57b73c1dec54c2108b
evgenjnr <evgeniyr7@gmail.com>
2022-04-26 11:25:28 +0300

    feat(notes): add notes components

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
 import { NotesPageComponent } from './page/notes-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotesReviewComponent } from './components/notes-review/notes-review.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
 
 
 @NgModule({
   declarations: [
-    NotesPageComponent
+    NotesPageComponent,
+    NotesReviewComponent,
+    NotesListComponent
   ],
   imports: [
     CommonModule,
-    NotesRoutingModule
+    NotesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class NotesModule { }
