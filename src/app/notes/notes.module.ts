04ee04a691402f45c07a3750d0a17fe35eeced28
evgenjnr <evgeniyr7@gmail.com>
2022-04-26 12:21:37 +0300

    feate(notes-module): add search component, add add note component

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -6,19 +6,25 @@ import { NotesPageComponent } from './page/notes-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NotesReviewComponent } from './components/notes-review/notes-review.component';
 import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { MatButtonModule } from '@angular/material/button';
+import { NoteSearchComponent } from './components/notes-list/components/note-search/note-search.component';
+import { NoteAddComponent } from './components/notes-list/components/note-add/note-add.component';
 
 
 @NgModule({
   declarations: [
     NotesPageComponent,
     NotesReviewComponent,
-    NotesListComponent
+    NotesListComponent,
+    NoteSearchComponent,
+    NoteAddComponent
   ],
   imports: [
     CommonModule,
     NotesRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule
   ]
 })
 export class NotesModule { }
