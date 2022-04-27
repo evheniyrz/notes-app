c4d80e2f5942bbb689cae07ebe6ca448f67a7799
evgenjnr <evgeniyr7@gmail.com>
2022-04-27 11:42:16 +0300

    feat(notes-module): delete note implementation

--- src/app/notes/components/notes-list/notes-list.component.ts
+++ src/app/notes/components/notes-list/notes-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { concatMap, map, Observable, tap } from 'rxjs';
 import { Note } from 'src/app/services/in-memory-db/models/note.model';
 import { WebSocketService } from 'src/app/services/web-socket/web-socket.service';
 import { WS_NOTE_EVENTS } from 'src/app/services/web-socket/ws-note-events';
+import { NoteApiService } from '../../services/note/note.service';
 
 @Component({
   selector: 'app-notes-list',
@@ -13,13 +14,23 @@ export class NotesListComponent implements OnInit {
 
   public noteList$: Observable<Note[]>;
 
-  constructor(private wsService: WebSocketService) {
+  constructor(private wsService: WebSocketService, private service: NoteApiService) {
     this.noteList$ = this.wsService.on(WS_NOTE_EVENTS.ON.UPDATE_DATA).pipe(
       map((resp) => { return this.getSortedNotes(resp as Note[]) })
     ) as Observable<Note[]>;
   }
 
   ngOnInit(): void {
+  }
+
+  public deleteNote(id: string): void {
+    this.service.deleteNote(id).pipe(
+      concatMap(() => {
+        return this.service.getNotes().pipe((
+          tap((response: Note[]) => this.wsService.send(WS_NOTE_EVENTS.SEND.UPDATE_DATA, response))
+        ))
+      })
+    ).subscribe();
   }
 
   private getSortedNotes(input: Note[]): Note[] {
